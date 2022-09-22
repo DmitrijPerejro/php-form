31960c7eef15cae73da21626cf94489b4e50da91
perejro <perejro@centro.team>
2022-09-22 13:43:51 +0300

    clear

--- server.php
+++ server.php
@@ -1,20 +1,17 @@
 <?php
-
-array_values($_POST["skills"]);
-
-echo "name: " . $_POST["name"];
-echo "<br>";
-echo "email: " . $_POST["email"];
-echo "<br>";
-echo "password: " . $_POST["password"];
-echo "<br>";
-echo "age: " . $_POST["age"];
-echo "<br>";
-echo "sex: " . $_POST["gender"];
-echo "<br>";
-echo "skills: ";
-foreach ($_POST["skills"] as $skill) {
-  echo "$skill ";
-}
-echo "<br>";
-echo "comment: " . $_POST["comment"];
+  echo "name: " . $_POST["name"];
+  echo "<br>";
+  echo "email: " . $_POST["email"];
+  echo "<br>";
+  echo "password: " . $_POST["password"];
+  echo "<br>";
+  echo "age: " . $_POST["age"];
+  echo "<br>";
+  echo "sex: " . $_POST["gender"];
+  echo "<br>";
+  echo "skills: ";
+  foreach ($_POST["skills"] as $skill) {
+    echo "$skill ";
+  }
+  echo "<br>";
+  echo "comment: " . $_POST["comment"];
